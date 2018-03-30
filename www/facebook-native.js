bf291574a64897c58e68e1dac92c7b3858a01516
Chris Williams <chrismw.dev@gmail.com>
2018-03-30 14:16:14 -0400

    Convert module exports functions into an object
    This will not change any functionality, but it will make it easier
    going forward to push more logic into javascript.

--- www/facebook-native.js
+++ www/facebook-native.js
@@ -1,22 +1,24 @@
 var exec = require('cordova/exec')
 
-exports.getLoginStatus = function getLoginStatus (s, f) {
+var facebookConnectPlugin = {}
+
+facebookConnectPlugin.getLoginStatus = function getLoginStatus (s, f) {
   exec(s, f, 'FacebookConnectPlugin', 'getLoginStatus', [])
 }
 
-exports.showDialog = function showDialog (options, s, f) {
+facebookConnectPlugin.showDialog = function showDialog (options, s, f) {
   exec(s, f, 'FacebookConnectPlugin', 'showDialog', [options])
 }
 
-exports.login = function login (permissions, s, f) {
+facebookConnectPlugin.login = function login (permissions, s, f) {
   exec(s, f, 'FacebookConnectPlugin', 'login', permissions)
 }
 
-exports.checkHasCorrectPermissions = function checkHasCorrectPermissions (permissions, s, f) {
+facebookConnectPlugin.checkHasCorrectPermissions = function checkHasCorrectPermissions (permissions, s, f) {
   exec(s, f, 'FacebookConnectPlugin', 'checkHasCorrectPermissions', permissions)
 }
 
-exports.logEvent = function logEvent (name, params, valueToSum, s, f) {
+facebookConnectPlugin.logEvent = function logEvent (name, params, valueToSum, s, f) {
   // Prevent NSNulls getting into iOS, messes up our [command.argument count]
   if (!params && !valueToSum) {
     exec(s, f, 'FacebookConnectPlugin', 'logEvent', [name])
@@ -29,36 +31,38 @@ exports.logEvent = function logEvent (name, params, valueToSum, s, f) {
   }
 }
 
-exports.logPurchase = function logPurchase (value, currency, s, f) {
+facebookConnectPlugin.logPurchase = function logPurchase (value, currency, s, f) {
   exec(s, f, 'FacebookConnectPlugin', 'logPurchase', [value, currency])
 }
 
-exports.getAccessToken = function getAccessToken (s, f) {
+facebookConnectPlugin.getAccessToken = function getAccessToken (s, f) {
   exec(s, f, 'FacebookConnectPlugin', 'getAccessToken', [])
 }
 
-exports.logout = function logout (s, f) {
+facebookConnectPlugin.logout = function logout (s, f) {
   exec(s, f, 'FacebookConnectPlugin', 'logout', [])
 }
 
-exports.api = function api (graphPath, permissions, s, f) {
+facebookConnectPlugin.api = function api (graphPath, permissions, s, f) {
   permissions = permissions || []
   exec(s, f, 'FacebookConnectPlugin', 'graphApi', [graphPath, permissions])
 }
 
-exports.appInvite = function appLinks (options, s, f) {
+facebookConnectPlugin.appInvite = function appLinks (options, s, f) {
   options = options || {}
   exec(s, f, 'FacebookConnectPlugin', 'appInvite', [options])
 }
 
-exports.getDeferredApplink = function (s, f) {
+facebookConnectPlugin.getDeferredApplink = function (s, f) {
   exec(s, f, 'FacebookConnectPlugin', 'getDeferredApplink', [])
 }
 
-exports.activateApp = function (s, f) {
+facebookConnectPlugin.activateApp = function (s, f) {
   exec(s, f, 'FacebookConnectPlugin', 'activateApp', [])
 }
 
-exports.getDeferredApplink = function (s, f) {
-	  exec(s, f, 'FacebookConnectPlugin', 'getDeferredApplink', [])
+facebookConnectPlugin.getDeferredApplink = function (s, f) {
+  exec(s, f, 'FacebookConnectPlugin', 'getDeferredApplink', [])
 }
+
+module.exports = facebookConnectPlugin;
